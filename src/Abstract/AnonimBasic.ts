3b5d6b6931e91299bb8999d71c8acd3f8f45298c
efeArdaYildirim <efeardayildirim@gmail.com>
2020-09-27 08:48:02 +0300

    anonim basic yazildi
    express yada diger basti frame vorkler icin

---
+++ src/Abstract/AnonimBasic.ts
@@ -0,0 +1,64 @@
+import { App } from '../Classes/App';
+import { DAL } from '../Classes/DAL';
+import { Validator } from '../Classes/Validator';
+import { User } from '../tipitipler/User';
+
+export abstract class AnonimBaisic {
+
+
+  db: DAL;
+  app: App;
+  constructor(dal: DAL) {
+    this.db = dal;
+    this.app = new App(dal);
+  }
+
+  private ValidateBaisicUserData(data: User): void {
+    new Validator(data)
+      .ItIsshouldToBeThere([
+        "landAgent",
+        "isLandAgent",
+        "name",
+        "email",
+        "password",
+        "yearOfBirdth",
+      ])
+      .MaxLength("name", 64)
+      .MinLength("name", 2)
+      .MaxWordCoud("name", 4)
+      .IsEmail("email")
+      .MinLength("password", 8)
+      .MaxLength("password", 64)
+      .IsBoolean("isLandAgent")
+      .IsNumber("yearOfBirdth");
+    new Validator(data.landAgent).ItIsshouldToBeThere([
+      "turkisIdNumber",
+      "firstName",
+      "lastName",
+      "phoneNumber",
+    ]);
+  }
+
+  async AddSatan(user: User): Promise<boolean> {
+    this.ValidateBaisicUserData(user);
+
+    if (!user.landAgent) throw new Error("eksik veri");
+    this.app.TurkisIdCheck(user.landAgent);
+
+    const isCreated = this.db.CreatUserToDB({ data: user });
+
+    if (isCreated) return isCreated;
+    return false;
+
+    /* // bu kod fire base de calismiyacak
+    this.app.sendMailToReciver({
+      to: user.email,
+      text: "heasbi dogrulamak icin tikla",
+      subject: "hesap dogrulama",
+      html: "",
+    });
+    */
+  }
+
+
+}
